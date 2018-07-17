bbf3e7016f7c76fd90f678460fc7ac19f7822aef
cliftonbest <clifton.best@gsa.gov>
2018-07-17 09:26:59 -0400

    Converted segments to functions

--- src/main/scala/RHTA_Main.scala
+++ src/main/scala/RHTA_Main.scala
@@ -1,77 +1,95 @@
 
+/*import NerDLPipeline._
+#import com.johnsnowlabs.nlp.annotator._
+import com.johnsnowlabs.nlp.base._
+import com.johnsnowlabs.util.Benchmark
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.NGram*/
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object RHTA_Main extends App{
-
+  val formatter = java.text.NumberFormat.getCurrencyInstance
+  val performatter = java.text.NumberFormat.getPercentInstance
   val spark = SparkSession.builder
     .master("local[*]")
     .appName("Security and Protection RHTA")
+    /*.config("spark.driver.memory", "12G")
+    .config("spark.kryoserializer.buffer.max","200M")
+    .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")*/
     .getOrCreate()
-  //.config("spark.driver.maxResultSize","4G")
-  //.config("spark.driver.memory", "2G")
-  //.config("spark.executor.memory", "2G")
 
+
+   /* .builder()
+    .appName("test")
+    .master("local[*]")
+    .config("spark.driver.memory", "12G")
+    .config("spark.kryoserializer.buffer.max","200M")
+    .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+    .getOrCreate()
+
+  */
+
+  import spark.implicits._
 
   spark.sparkContext.setLogLevel("ERROR")
 
-  import spark.implicits._
+
 
   val input = spark
     .read
     .option("header", "true")
     .option("inferSchema", "true")
-    .option("sep", "\t").csv("/Users/destiny/Documents/development/GWCM_Exports/GWCM_FPDS_CURRENT_MONTH_JUNE_2018.tsv")
+    .option("sep", "\t").csv("/Users/cliftonbest/Documents/Development/R/GWCM_Exports/GWCM_FPDS_CURRENT_MONTH_JUNE_2018.tsv")
 
-  val s_and_p_ammo = input
-    .filter($"date_signed" >= "2017-10-01")
-  //.filter($"level_1_category" === "Security and Protection")
-
-  val formatter = java.text.NumberFormat.getCurrencyInstance
-  val RHTA_obs = s_and_p_ammo
-    .filter( $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)")
-    .select( $"dollars_obligated")
-    .agg(sum($"dollars_obligated")).first().getDouble(0)
-  println("FY18 RHTA market " + formatter.format(RHTA_obs))
+  val daterange_df = input.filter($"date_signed" >= "2016-10-01" && $"date_signed" <= "2017-09-30" )
+  val rhta_df = daterange_df.filter( $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)")
 
 
-  val RHTA_count = s_and_p_ammo
-    .filter( $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)")
-    .select($"dollars_obligated")
-    .count()
-  println("FY18 RHTA row count " + RHTA_count)
 
-  //1. Calculate the fuzzy addressable market for RHTA by aggregating the transactions that actually came from RHTA and adding the transactions harvested
-  //   from the descrption of requirements field using the regex criteria below
-  //val rhta_regex = ".*(?i)(train|reduced haz+ard|lead free).*"
-  //val formatter = java.text.NumberFormat.getCurrencyInstance
-  val rhta_regex = ".*(?i)(rhta|reduced haz+ard training ammunition|green ammo|haz+ard training ammunition|ammunition rhta|haz+ard frangible|rh frangible|lead free ammunition|training ammunition).*"
-  val ammo_rhta_addressable_market_df = s_and_p_ammo.filter($"description_of_requirement".rlike(rhta_regex)) //|| $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)")
-    .select($"description_of_requirement",$"contract_name", $"dollars_obligated", $"level_1_category", $"product_or_service_description")
-  val ammo_rhta_addressable_market_obs = ammo_rhta_addressable_market_df.agg(sum($"dollars_obligated")).first().getDouble(0)
-  println("FY18 Re-shaped RHTA addressable market " + formatter.format(ammo_rhta_addressable_market_obs))
+  //RHTA_Actuals(input)
+  RHTA_filter_addressable(input)
 
-  //Break out the obligations RHTA obligations in to L1 cat and psc
-  val RHTA_breakout_by_obs = s_and_p_ammo
-    .filter( $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)")
-    .groupBy($"level_1_category", $"product_or_service_description").agg(sum("dollars_obligated")).show()
+  def RHTA_Actuals(local_input: org.apache.spark.sql.DataFrame) = {
 
-  //2. Provide re-shaped RHTA addressable composition: Breakout scope makes it clear that regex alone is not a rigid enough heuristic to characterize RHTA
-  //   The addressable market is overinflated by all categories in which dont involve ammunition
-  //   Corrective recommendation is to shape from the beginning to limit the input dataframe to level_1_category_group = "GWCM"
-  val RHTA_addressable_market_breakout_by_obs = ammo_rhta_addressable_market_df
-    .groupBy($"level_1_category", $"product_or_service_description").agg(sum("dollars_obligated")).show()
+    val RHTA_obs = local_input.select( $"dollars_obligated").agg(sum($"dollars_obligated")).first().getDouble(0)
+    println("FY18 RHTA market " + formatter.format(RHTA_obs))
 
-  //3. Calcuate the percentage utilization of RHTA in respect to the total addressable market for RHTA
+    val RHTA_count = local_input.select($"dollars_obligated").count()
+    println("FY18 RHTA row count " + RHTA_count)
 
-  val rhta_addressable_market_cal = RHTA_obs / ammo_rhta_addressable_market_obs * 100
+    //Break out the obligations RHTA obligations in to L1 cat and psc
+    val RHTA_breakout_by_obs = local_input.groupBy($"level_1_category",$"product_or_service_code", $"product_or_service_description").agg(sum("dollars_obligated")).show(truncate = false)
 
-  //val performatter = java.text.NumberFormat.getPercentInstance
-
-  println("FY18 RHTA obligations " + formatter.format(RHTA_obs))
-  println("FY18 RHTA percent utilization " + rhta_addressable_market_cal)
+    //Break out the obligations RHTA obligations in to L1 cat and psc
+    val RHTA_breakout_by_count = local_input.groupBy($"level_1_category",$"product_or_service_code", $"product_or_service_description").agg(count($"product_or_service_code")).show(truncate = false)
+  }
 
 
-  ammo_rhta_addressable_market_df.repartition(1).write.option("header", "true").csv("/Users/destiny/rhta_regex_df2.csv")
+  def RHTA_filter_addressable(local_input: org.apache.spark.sql.DataFrame): Unit = {
+
+    val rhta_regex = ".*(?i)(rhta|reduced haz+ard training ammunition|ammunition rhta|frangible).*"
+
+    val ammo_rhta_addressable_market_df = local_input.filter($"description_of_requirement".rlike(rhta_regex) )
+
+    // || $"contract_name" === "Reduced Hazard Training Ammunition (RHTA)").select($"description_of_requirement",$"contract_name", $"dollars_obligated", $"level_1_category", $"product_or_service_code",$"product_or_service_description")
+
+    val addressable_obs_grouping = ammo_rhta_addressable_market_df.
+      groupBy($"level_1_category",$"product_or_service_code", $"product_or_service_description").
+      agg(sum("dollars_obligated")).show(truncate = false)
+
+    val addressable_count_grouping = ammo_rhta_addressable_market_df.
+      groupBy($"level_1_category",$"product_or_service_code", $"product_or_service_description").
+      agg(count($"product_or_service_code")).show(truncate = false)
+
+    val ammo_rhta_addressable_market_obs = ammo_rhta_addressable_market_df.select( $"dollars_obligated").agg(sum($"dollars_obligated")).first().getDouble(0)
+    println("FY18 Re-shaped RHTA addressable market " + formatter.format(ammo_rhta_addressable_market_obs))
+
+
+  }
+
+
 
 }
+
+
